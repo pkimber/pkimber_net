d448ba3975f52c107b0cbde5cf0b1d43373a3b72
Patrick Kimber <code@pkimber.net>
2013-08-23 16:59:55 +0100

    version 0.0.02

---
+++ setup.py
@@ -0,0 +1,49 @@
+import os
+from distutils.core import setup
+
+
+def read_file_into_string(filename):
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+def get_readme():
+    for name in ('README', 'README.rst', 'README.md'):
+        if os.path.exists(name):
+            return read_file_into_string(name)
+    return ''
+
+
+setup(
+    name='pkimber-pkimber-net',
+    packages=['project', 'project.templatetags', 'project.management', 'project.management.commands', 'settings'],
+    package_data={
+        'project': [
+            'static/*.*',
+            'static/img/*.*',
+            'static/img/project/*.*',
+            'templates/*.*',
+            'templates/project/*.*',
+        ],
+    },
+    version='0.0.02',
+    description='my web site',
+    author='Patrick Kimber',
+    author_email='code@pkimber.net',
+    url='git@github.com:pkimber/pkimber_net.git',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Office/Business :: Scheduling',
+    ],
+    long_description=get_readme(),
+)
