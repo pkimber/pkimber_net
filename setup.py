8811306c95ee17600aecdcc72b3c5c2b093317a2
Patrick Kimber <code@pkimber.net>
2013-09-09 11:45:23 +0100

    version 0.0.16

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
             'templates/project/*.*',
         ],
     },
-    version='0.0.15',
+    version='0.0.16',
     description='my web site',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
