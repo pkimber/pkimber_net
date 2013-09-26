4f552381aa94b5d64bc9e07f5cde1012973629cb
Patrick Kimber <code@pkimber.net>
2013-09-26 21:44:40 +0100

    postgres test data

--- settings/dev_patrick.py
+++ settings/dev_patrick.py
@@ -2,11 +2,22 @@ from .local import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'temp.db',                               # Or path to database file if using sqlite3.
-        'USER': '',                               # Not used with sqlite3.
-        'PASSWORD': '',                           # Not used with sqlite3.
-        'HOST': '',                              # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                                          # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pkimber_net_20130926_170320_patrick',
+        'USER': 'patrick',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'temp.db',                               # Or path to database file if using sqlite3.
+#        'USER': '',                               # Not used with sqlite3.
+#        'PASSWORD': '',                           # Not used with sqlite3.
+#        'HOST': '',                              # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                                          # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
