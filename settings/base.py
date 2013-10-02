1d37abd5a83d99905c5eb58671064d0b6343025d
Patrick Kimber <code@pkimber.net>
2013-10-02 18:52:32 +0100

    disable 'sites' so registration uses 'RequestSite' for domain

--- settings/base.py
+++ settings/base.py
@@ -140,7 +140,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable admin documentation:
