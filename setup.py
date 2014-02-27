7383d37f0ba106924a52362b95ed3e26bfd3b7f8
Patrick Kimber <code@pkimber.net>
2014-02-27 13:40:59 +0100

    version 0.0.54

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
             'templates/project/*.*',
         ],
     },
-    version='0.0.53',
+    version='0.0.54',
     description='my web site',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
