51f1003e60a287066a585e7aac29bc3e9cd8efc5
Patrick Kimber <code@pkimber.net>
2014-07-08 20:33:55 +0100

    tidy

--- web/urls.py
+++ web/urls.py
@@ -1,3 +1,6 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.conf import settings
 from django.conf.urls import (
     include,
--- web/views.py
+++ web/views.py
@@ -1,3 +1,6 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.views.generic import ListView, TemplateView
 
 from braces.views import (
