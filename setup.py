714cf7658c7ec0fcff33fb1de1d25ab80196c6aa
Patrick Kimber <code@pkimber.net>
2014-08-04 16:35:35 +0100

    version 0.1.18

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
             'templates/web/*.*',
         ],
     },
-    version='0.1.17',
+    version='0.1.18',
     description='my web site',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
