a2acfdd7839c231fe064c6fb15db71a3b38d1277
Patrick Kimber <code@pkimber.net>
2015-03-18 17:06:48 +0100

    version 0.1.30

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
             'templates/dash/*.*',
         ],
     },
-    version='0.1.29',
+    version='0.1.30',
     description='my web site',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
