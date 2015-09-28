b184e00a6d8d4640bfdefe225025accd07a94540
Patrick Kimber <code@pkimber.net>
2015-09-28 20:14:01 +0100

    prep for release

--- settings/dev_patrick.py
+++ settings/dev_patrick.py
@@ -1,24 +1,25 @@
+# -*- encoding: utf-8 -*-
 from .local import *
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'test_pkimber_net_patrick',
-#        'USER': 'patrick',
-#        'PASSWORD': '',
-#        'HOST': '',
-#        'PORT': '',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'temp.db',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_pkimber_net_patrick',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'temp.db',
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': '',
+#        'PORT': '',
+#    }
+#}
