5539addf5ff0320af90e0412e059c773cc06d52c
Malcolm Dinsmore <zebyea@gmail.com>
2016-12-08 12:49:04 +0000

    Update dev env

--- settings/dev_malcolm.py
+++ settings/dev_malcolm.py
@@ -1,23 +1,23 @@
 from .local import *
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'test_pkimber_net_patrick',
-#        'USER': 'patrick',
-#        'PASSWORD': '',
-#        'HOST': '',
-#        'PORT': '',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'temp.db',
-        'USER': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dev_www_pkimber_net_malcolm',
+        'USER': 'malcolm',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'temp.db',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
