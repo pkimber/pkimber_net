a82a95289d90507490858aaa44ecaf5bdc6fdde7
Patrick Kimber <patrick@kbsoftware.co.uk>
2022-06-24 17:24:31 +0100

    version 0.1.64

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
             'templates/dash/*.*',
         ],
     },
-    version='0.1.63',
+    version='0.1.64',
     description='my web site',
     author='Patrick Kimber',
     author_email='patrick@kbsoftware.co.uk',
